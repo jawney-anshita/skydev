80303acf860e0320c1ee5f603a69702de9f21b28
jawney-anshita <jawney.anshita@gmail.com>
2023-12-09 18:31:28 +0530

    image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-}
-
-module.exports = nextConfig
+    images: {
+      loader: 'custom',
+      loaderFile: './my-loader.ts',
+    },
+  }
+   
+  module.exports = nextConfig
